0eb8a3518842d87d4ba4d1ea11746b619947b8b4
AleNieva <93145001+AleNieva@users.noreply.github.com>
2024-01-18 19:38:56 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
-let nombre = prompt ("Ingrese nombre del alumno")
-let nota = prompt ("Ingrese cantidad de notas que tiene")
-let suma=0
-let promedio =0
+function calcularPromedio(notas) {
+    let suma = 0;
 
+    for (let i = 1; i <= notas; i++) {
+        let numero = prompt("Ingrese la nota " + i);
+        suma += parseFloat(numero);
+    }
 
-for ( i=1; i<= nota; i++) {
-    numero = prompt ("Ingrese la nota")
-     suma = parseFloat(suma)+ parseFloat(numero)
-    
+    return suma / notas;
 }
 
-promedio = suma / numero
 
-alert ("El promedio es " + promedio)
+let nombre = prompt ("Ingrese nombre del alumno");
+let notas = prompt ("Ingrese cantidad de notas que tiene");
+
+let promedio = calcularPromedio(notas);
+
+
+alert ("El promedio es " + promedio.toFixed(1));
 
 if (promedio >=6) {
-    alert("Felicidades " + nombre + " aprobo con " + promedio)
+    alert("Felicidades " + nombre + " aprobo con " + promedio.toFixed(1));
   
     
 } else {
-    alert(nombre + " va a recuperatorio con " + promedio)
+    alert(nombre + " va a recuperatorio con " + promedio.toFixed(1));
 }
